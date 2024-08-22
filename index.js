0959617c4e46004408e57fd21766b1b4d65db92c
Dong Shikai <2254566697@qq.com>
2024-08-22 14:06:45 +0800

    修改
    css hover属性，
    显示与隐藏动态变化条

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ document.addEventListener('DOMContentLoaded', function() {
     // 获取音乐元素和按钮
     const audioElement = document.getElementById('backgroundMusic');
     const playPauseButton = document.getElementById('playPauseButton');
+    const bounce = document.getElementById('bounce');
     // 初始状态为播放
     let isPlaying = true;
     audioElement.play();
@@ -15,11 +16,13 @@ document.addEventListener('DOMContentLoaded', function() {
             // 如果音乐没有播放，则开始播放
             audioElement.play();
             playPauseButton.textContent = '暂停';
+            bounce.style.visibility='visible';
             isPlaying = true;
         } else {
             // 如果音乐正在播放，则暂停播放
             audioElement.pause();
             playPauseButton.textContent = '播放';
+            bounce.style.visibility='hidden';
             isPlaying = false;
         }
     });
