4eb270a366f2e8b3e784b33cefbd06a8fd483540
Dong Shikai <2254566697@qq.com>
2024-08-22 14:50:12 +0800

    焦点视图

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-document.getElementById('confirmButton').addEventListener('click', function() {
-    var overlay = document.getElementById('confirmationOverlay');
-    overlay.style.display = 'none';
+document.getElementById('confirmButton').addEventListener('click', function () {
+    document.getElementById('confirmationOverlay').style.visibility='hidden';
 });
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     // 获取音乐元素和按钮
     const audioElement = document.getElementById('backgroundMusic');
     const playPauseButton = document.getElementById('playPauseButton');
@@ -11,21 +10,24 @@ document.addEventListener('DOMContentLoaded', function() {
     let isPlaying = true;
     audioElement.play();
     // 添加事件监听器
-    playPauseButton.addEventListener('click', function() {
+    playPauseButton.addEventListener('click', function () {
         if (!isPlaying) {
             // 如果音乐没有播放，则开始播放
             audioElement.play();
             playPauseButton.textContent = '暂停';
-            bounce.style.visibility='visible';
+            bounce.style.visibility = 'visible';
             isPlaying = true;
         } else {
             // 如果音乐正在播放，则暂停播放
             audioElement.pause();
             playPauseButton.textContent = '播放';
-            bounce.style.visibility='hidden';
+            bounce.style.visibility = 'hidden';
             isPlaying = false;
         }
     });
 
     // 处理确认按钮的点击事件
+});
+window.addEventListener('blur', function () {
+    document.getElementById('confirmationOverlay').style.visibility='visible';
 });
